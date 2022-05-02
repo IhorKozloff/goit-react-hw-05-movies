1304cd91d254d4f542d1f3bdcc011fce4b35e73e
IhorKozloff <ik.eyeshield21@gmail.com>
2022-05-02 23:24:19 +0300

    стили ориентировочные убрал

--- src/components/GlobalStyles/GlobalStyles.styled.jsx
+++ src/components/GlobalStyles/GlobalStyles.styled.jsx
@@ -2,7 +2,6 @@ import styled from '@emotion/styled'
 
 export const Container = styled.div`
     width: 984px;
-    border: 1px solid black;
     margin: 0 auto;
 `;
 
@@ -10,5 +9,4 @@ export const Section = styled.section`
     margin: 0 auto;
     width: 1024px;
     padding-top: 20px;
-    background-color: pink;
 `;
--- src/components/HeaderBar/HeaderBar.styled.jsx
+++ src/components/HeaderBar/HeaderBar.styled.jsx
@@ -4,7 +4,6 @@ export const SectionHeader = styled.header`
     margin: 0 auto;
     width: 1024px;
     padding-top: 20px;
-    background-color: aqua;
     display: flex;
     justify-content: center;
     align-items: center;
