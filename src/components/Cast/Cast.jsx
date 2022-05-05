eef287c8089cfbf401487a5c177b6a3313f5d541
IhorKozloff <ik.eyeshield21@gmail.com>
2022-05-05 13:57:24 +0300

    noname-avatar fixed

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
-import { CastWrapper } from 'components/Cast/Cast.styled';
+import { CastWrapper, NonameAvatarWrapper } from 'components/Cast/Cast.styled';
 import { searchAPIActors } from 'components/API';
+import  male  from 'img/male.jpg'
 
 
 
@@ -9,6 +10,7 @@ export default function Cast () {
     
   const [actorsListData, setActorsListData] = useState(null);
   const { movieId } = useParams();
+
     
     useEffect(() => {
         searchAPIActors(movieId).then(data => {
@@ -27,12 +29,23 @@ export default function Cast () {
                         return (
                             <li key={item.id}>
 
-                                <img 
-                                    src={`https://image.tmdb.org/t/p/original${item.profile_path}`}
-                                    width="60"
-                                    height="80"
-                                    alt=""
-                                /> 
+                                {
+                                    item.profile_path !== null 
+                                    ?   <img 
+                                            src={`https://image.tmdb.org/t/p/original${item.profile_path}`}
+                                            width="60"
+                                            height="80"
+                                            alt="actors-avatar"
+                                        /> 
+                                    :   <NonameAvatarWrapper>
+                                            <img 
+                                                src={`${male}`}
+                                                width="60"
+                                                height="60"
+                                                alt="noname-avatar"
+                                            />
+                                        </NonameAvatarWrapper>
+                                }
                                 
                                 <p>{item.name}</p>
                                 <p>Character: {item.character}</p>
--- src/components/Cast/Cast.styled.jsx
+++ src/components/Cast/Cast.styled.jsx
@@ -3,3 +3,6 @@ import styled from '@emotion/styled';
 export const CastWrapper = styled.div`
 
 `;
+export const NonameAvatarWrapper = styled.div`
+
+`;
